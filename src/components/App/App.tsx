b3e6c8b9e3ced5992d097e4306487f1a215be4a3
ТАЗДМБ ТАЗДМБ <fhdbudhh@gmail.com>
2023-01-13 17:39:26 +0300

    fix/replaced app and other

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,14 +3,14 @@ import logo from '../../logo.svg';
 import AppHeader from '../app-header/app-header'
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
-import './App.css';
-import Data from '../utils/data';
+import style from './app.module.css';
+import Data from '../../utils/data';
 
 
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.App}>
       <AppHeader />
       <BurgerIngredients data={Data} />
       <BurgerConstructor />
--- src/components/navigation-link/navigation-link.js
+++ src/components/navigation-link/navigation-link.js
@@ -1,5 +1,6 @@
 import React from "react";
-import style from './navigation-link.module.css'
+import PropTypes from "prop-types";
+import style from "./navigation-link.module.css";
 
 class NavigationLink extends React.Component {
   constructor(props) {
@@ -7,14 +8,24 @@ class NavigationLink extends React.Component {
   }
   render() {
     return (
-    <div className={style.container + ' ' + (this.props.flex ? '' : style.profileFlex) + ' pl-5 pr-5 pt-4 pb-4'}>
-      {this.props.children}
-      <p className="text text_type_main-default pl-2">
-        {this.props.text}
-      </p>
-    </div>
+      <div
+        className={
+          style.container +
+          " " +
+          (this.props.flex ? "" : style.profileFlex) +
+          " pl-5 pr-5 pt-4 pb-4"
+        }
+      >
+        {this.props.children}
+        <p className="text text_type_main-default pl-2">{this.props.text}</p>
+      </div>
     );
   }
 }
+
+NavigationLink.propTypes = {
+  text: PropTypes.string,
+  chikdren: PropTypes.elementType,
+};
 
 export default NavigationLink;
